1215abff92b6265c5fe8c9bc7a3ee4bc445a20cf
jikerlimbaga123-eng <jikerlimbaga123-eng@gmail.com>
2026-05-25 01:36:46 +0800

    Fix photo imports

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -30,6 +30,12 @@ import p16 from "../assets/p16.jpg";
 import p17 from "../assets/p17.jpg";
 import p18 from "../assets/p18.jpg";
 import p19 from "../assets/p19.jpg";
+import p20 from "../assets/p20.jpg";
+import p21 from "../assets/p21.jpg";
+import p22 from "../assets/p22.jpg";
+import p23 from "../assets/p23.jpg";
+import p24 from "../assets/p24.jpg";
+import p25 from "../assets/p25.jpg";
 
 const Home = () => {
     const [Active, SetActive] = useState(true);
@@ -41,7 +47,7 @@ const Home = () => {
     const playerRef = useRef(null);
     const galleryPlayerRef = useRef(null);
 
-    const galleryPhotos = [p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18,p19];
+    const galleryPhotos = [p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18,p19,p20,p21,p22,p23,p24,p25];
 
     const gifts = [
         {
